467e1bbbf4e0d13173a26ea7e6b13d1ff4b8dae3
Nate Hamilton <nathaniel_hamilton@outlook.com>
2020-04-22 16:44:02 -0500

    cleaning code

--- reproduce_models.bash
+++ reproduce_models.bash
@@ -5,11 +5,15 @@ python ddpg.py --random-seed 8 --log-path './rs_8'
 python ddpg.py --random-seed 1964 --log-path './rs_1964'
 python ddpg.py --random-seed 1754 --log-path './rs_1754'
 
+python ddpg.py --random-seed 8 --constrain 1 --log-path './constrained_rs_8'
+python ddpg.py --random-seed 1964 --constrain 1 --log-path './constrained_rs_1964'
+python ddpg.py --random-seed 1754 --constrain 1 --log-path './constrained_rs_1754'
+
 cd ../DDPG-CBF
 cd
 python ddpg.py --random-seed 1964 --log-path './rs_1964'
 python ddpg.py --random-seed 1754 --log-path './rs_1754'
 
 cd ..
-python plot_results.py --folder1 'rs_8' --folder2 'rs_1964' --folder3 'rs_1754'
-python generate_table.py --folder1 'rs_8' --folder2 'rs_1964' --folder3 'rs_1754'
+python plot_results.py
+python generate_table.py
